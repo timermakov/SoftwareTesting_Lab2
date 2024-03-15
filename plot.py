c8bda15181c637aafd2995e2db5603c94387aef0
timermakov <ts.ermakov@yandex.ru>
2024-03-15 05:42:11 +0300

    Add other files

---
+++ plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+csv_file_path = 'Output.csv'
+
+data = pd.read_csv(csv_file_path, header=None, names=['x', 'y'])
+
+plt.figure(figsize=(10, 6))
+plt.plot(data['x'], data['y'], marker='o', linestyle='-', color='b')
+plt.title('Plot of CSV Data')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid(True)
+plt.show()
