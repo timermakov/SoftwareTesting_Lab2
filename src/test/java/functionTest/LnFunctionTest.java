13bf13b25a0f0a0004322c2793aa5eb0e74a0945
timermakov <ts.ermakov@yandex.ru>
2024-03-15 10:59:41 +0300

    Mockito stubs added

--- src/test/java/functionTest/LnFunctionTest.java
+++ src/test/java/functionTest/LnFunctionTest.java
@@ -3,6 +3,9 @@ package functionTest;
 import function.LnFunction;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class LnFunctionTest {
 
@@ -11,16 +14,28 @@ public class LnFunctionTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testLnFunctionAtZero() {
+        LnFunction lnMock = mock(LnFunction.class);
+
+        when(lnMock.ln(eq(0.))).thenThrow(IllegalArgumentException.class);
+
         lnFunction.ln(0);
     }
 
     @Test
     public void testLnFunctionAtOne() {
+        LnFunction lnMock = mock(LnFunction.class);
+
+        when(lnMock.ln(eq(1))).thenReturn(0.);
+
         assertEquals(0, lnFunction.ln(1), PRECISION);
     }
 
     @Test
     public void testLnFunctionAtE() {
+        LnFunction lnMock = mock(LnFunction.class);
+
+        when(lnMock.ln(eq(Math.E))).thenReturn(1.);
+
         assertEquals(1, lnFunction.ln(Math.E), PRECISION);
     }
 }
--- src/test/java/functionTest/SinFunctionTest.java
+++ src/test/java/functionTest/SinFunctionTest.java
@@ -4,6 +4,9 @@ import function.SinFunction;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SinFunctionTest {
 
@@ -12,21 +15,37 @@ public class SinFunctionTest {
 
     @Test
     public void testSinFunctionAtZero() {
+        SinFunction sinMock = mock(SinFunction.class);
+
+        when(sinMock.sin(eq(0))).thenReturn(0.);
+
         assertEquals(0, sinFunction.sin(0), PRECISION);
     }
 
     @Test
     public void testSinFunctionAtPiOverTwo() {
+        SinFunction sinMock = mock(SinFunction.class);
+
+        when(sinMock.sin(eq(Math.PI / 2))).thenReturn(1.);
+
         assertEquals(1, sinFunction.sin(Math.PI / 2), PRECISION);
     }
 
     @Test
     public void testSinFunctionAtPi() {
+        SinFunction sinMock = mock(SinFunction.class);
+
+        when(sinMock.sin(eq(Math.PI))).thenReturn(0.);
+
         assertEquals(0, sinFunction.sin(Math.PI), PRECISION);
     }
 
     @Test
     public void testSinFunctionAtThreePiOverTwo() {
+        SinFunction sinMock = mock(SinFunction.class);
+
+        when(sinMock.sin(eq(3 * Math.PI / 2))).thenReturn(-1.);
+
         assertEquals(-1, sinFunction.sin(3 * Math.PI / 2), PRECISION);
     }
 
